9feac23ad17a9489bfdd94a219b9dafff5d14038
FAR <fer.antonio.ruiz@gmail.com>
2025-08-26 10:17:45 -0600

    feat: bundle recipe service chunk

--- src/js/item-loader.js
+++ src/js/item-loader.js
@@ -16,7 +16,9 @@ async function ensureDeps() {
     depsPromise = Promise.all([
       import('./items-core.js'),
       import('./utils/priceHelper.js'),
-        import('./services/recipeService.js'),
+      import('./services/recipeService.js').catch(() =>
+        import('./services/recipeService.min.js')
+      ),
       import('./utils/stateManager.js')
     ]).then(([core, price, recipe, state]) => {
       ({
