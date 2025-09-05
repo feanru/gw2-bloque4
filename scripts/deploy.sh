dda75862634927ee3d27c570ed89bcb33d73c3cd
FAR <fer.antonio.ruiz@gmail.com>
2025-09-04 22:42:58 -0600

    feat: add static release directory to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,11 @@ TARGET=/var/www/gw2
 RELEASE=${1:-$(git rev-parse --short HEAD)}
 KEEP=${KEEP:-3}
 
-mkdir -p "$TARGET/releases/$RELEASE"
-cp -a dist/. "$TARGET/releases/$RELEASE/"
-ln -sfn "$TARGET/releases/$RELEASE" "$TARGET/current"
+mkdir -p "$TARGET/static/releases/$RELEASE"
+cp -a dist css img service-worker.min.js *.html "$TARGET/static/releases/$RELEASE/"
+ln -sfn "$TARGET/static/releases/$RELEASE" "$TARGET/static/current"
 
-cd "$TARGET/releases"
+cd "$TARGET/static/releases"
 ls -1t | tail -n +$((KEEP+1)) | xargs -r rm -rf
 
 # Purge CDN cache to force revalidation of assets
